8fc9fd9255a0a84ab1baac1f858db2c559033a8c
CKline22 <CKline.1022@gmail.com>
2022-08-18 17:40:31 -0400

    routes inprogress

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,11 +1,21 @@
 import NavbarComponent from './components/Navbar';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+//Pages
+import Home from './pages/homePage'
+import About from './pages/aboutPage'
+import Contact from './pages/contactPage'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <NavbarComponent/>
-    </div>
+      <Routes>
+        <Route path="/" exact component={Home}/>
+        <Route path="/about" exact component={About}/>
+        <Route path="/contact" exact component={Contact}/>
+      </Routes>
+    </Router>
   );
 }
 
