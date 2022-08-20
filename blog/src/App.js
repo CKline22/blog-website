0af049ca6eeeb502f9f4680257b196f102709f4d
CKline22 <CKline.1022@gmail.com>
2022-08-20 10:12:02 -0400

    footer added

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,4 +1,5 @@
 import NavbarComponent from './components/Navbar';
+import Footer from './components/footer';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 //Pages
 import Home from './pages/homePage'
@@ -10,6 +11,8 @@ import './App.css';
 
 function App() {
   return (
+    <div className='App-container'>
+      <div className='content-wrap'>
     <Router>
       <NavbarComponent/>
       <Routes>
@@ -20,6 +23,9 @@ function App() {
         <Route path="/gear" element={<Gear/>}/>
       </Routes>
     </Router>
+    </div>
+    <Footer/>
+    </div>
   );
 }
 
--- blog/src/components/Navbar.js
+++ blog/src/components/Navbar.js
@@ -10,7 +10,7 @@ function NavbarComponent () {
     return(
         <Navbar bg="dark" variant="dark" sticky="top" expand="lg">
             <Container>
-                <Navbar.Brand href="/">Outdoors</Navbar.Brand>
+                <Navbar.Brand href="/">The Outdoors Blog</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
