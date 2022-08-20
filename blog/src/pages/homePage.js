dd56b7b0b73a5ae637b68064789653738d138a20
CKline22 <CKline.1022@gmail.com>
2022-08-20 12:33:19 -0400

    cards created

--- blog/src/pages/homePage.js
+++ blog/src/pages/homePage.js
@@ -10,11 +10,46 @@ function Home () {
         <div>
             <Header/>
             <div>
-                <Row xs={1} md={2} className="g-4">
-                    <Col></Col>
-                    <Col></Col>
+                <Row xs={1} md={2}>
+                    <Col>
+                    <Card>
+                    <Card.Img variant="top" src="holder.js/100px160" />
+                        <Card.Body>
+                            <Card.Title>Camping Adventures</Card.Title>
+                            <Card.Text></Card.Text>
+                        </Card.Body>
+                    </Card>
+                    </Col>
+                    <Col>
+                    <Card>
+                    <Card.Img variant="top" src="holder.js/100px160" />
+                        <Card.Body>
+                            <Card.Title>Hiking Adventures</Card.Title>
+                            <Card.Text></Card.Text>
+                        </Card.Body>
+                    </Card>
+                    </Col>
                 </Row>
-                <Row></Row>
+                <Row xs={1} md={2}>
+                <Col>
+                    <Card>
+                    <Card.Img variant="top" src="holder.js/100px160" />
+                        <Card.Body>
+                            <Card.Title>Photography</Card.Title>
+                            <Card.Text></Card.Text>
+                        </Card.Body>
+                    </Card>
+                    </Col>
+                    <Col>
+                    <Card>
+                    <Card.Img variant="top" src="holder.js/100px160" />
+                        <Card.Body>
+                            <Card.Title>Travel</Card.Title>
+                            <Card.Text></Card.Text>
+                        </Card.Body>
+                    </Card>
+                    </Col>
+                </Row>
             </div>
         </div>
         
