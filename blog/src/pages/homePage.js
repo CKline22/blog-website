0a770c772607b9e00aa554ca117a37d6277dd690
CKline22 <CKline.1022@gmail.com>
2022-08-20 12:25:16 -0400

    home rows added

--- blog/src/pages/homePage.js
+++ blog/src/pages/homePage.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import Header from '../components/header';
+import CardGroup from 'react-bootstrap/CardGroup';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 function Home () {
     return(
         <div>
             <Header/>
-            <div>Home</div>
+            <div>
+                <Row xs={1} md={2} className="g-4">
+                    <Col></Col>
+                    <Col></Col>
+                </Row>
+                <Row></Row>
+            </div>
         </div>
         
     )
