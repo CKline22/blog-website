6f57e532f78c5e4bd5d15c5bc5279345ee239afc
CKline22 <CKline.1022@gmail.com>
2022-08-20 08:19:14 -0400

    nav updated

--- blog/src/App.js
+++ blog/src/App.js
@@ -4,6 +4,8 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages/homePage'
 import About from './pages/aboutPage'
 import Contact from './pages/contactPage'
+import Explore from './pages/explorePage';
+import Gear from './pages/gearPage';
 import './App.css';
 
 function App() {
@@ -14,6 +16,8 @@ function App() {
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
         <Route path="/contact" element={<Contact/>}/>
+        <Route path="/explore" element={<Explore/>}/>
+        <Route path="/gear" element={<Gear/>}/>
       </Routes>
     </Router>
   );
--- blog/src/components/Navbar.js
+++ blog/src/components/Navbar.js
@@ -2,22 +2,32 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 
 
 function NavbarComponent () {
     return(
-        <Navbar bg="dark" variant="dark" sticky="top">
+        <Navbar bg="dark" variant="dark" sticky="top" expand="lg">
             <Container>
-                <Navbar.Brand href="#home">Blog</Navbar.Brand>
+                <Navbar.Brand href="/">Outdoors</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#about">About</Nav.Link>
-                    <Nav.Link href="#contact">Contact</Nav.Link>
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="About">About</Nav.Link>
+                    <Nav.Link href="Contact">Contact</Nav.Link>
+                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="explore">Recommended Places to Explore</NavDropdown.Item>
+                        <NavDropdown.Item href="gear">
+                            Gear We Like
+                        </NavDropdown.Item>
+                            </NavDropdown>
                 </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
-    )
+    );
 }
 
 export default NavbarComponent; 
