b320f5ab107324f8bc6a9cf7a042776c1c0db4fd
CKline22 <CKline.1022@gmail.com>
2022-08-20 10:35:16 -0400

    header started

--- blog/src/pages/homePage.js
+++ blog/src/pages/homePage.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from '../components/header';
 
 function Home () {
     return(
         <div>
+            <Header/>
             <div>Home</div>
         </div>
         
