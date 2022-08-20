a0d96a902d78011d48505697c37750bb437b0fd4
CKline22 <CKline.1022@gmail.com>
2022-08-20 12:12:04 -0400

    about page updated

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
-Blog-wwebsite 
+Blog-website 
 Is an blog website about the outdoors and travel. I like to travel and explore nature, which is my inspration for this website. 
 
 
 Start by cloning the git repository, with git clone. then cd "" into blog-website. Then cd "blog". Now it's time to install the npm packages. run npm install. After the modules are installed, run npm start.
 
 This app uses:
-React
-React-bootstrap
-React-dom
-React-router-dom
-Bootstrap
-html
-javascript
+React,
+React-bootstrap,
+React-dom,
+React-router-dom,
+Bootstrap,
+html,
+javascript,
 css
--- blog/src/pages/aboutPage.js
+++ blog/src/pages/aboutPage.js
@@ -3,11 +3,16 @@ import Card from 'react-bootstrap/Card';
 
 function About () {
     return(
-        <div>
+        <div style={{ display: 'flex', justifyContent: 'center'}}>
             <Card>
-               <Card.Img src="/landscape-tent-water.jpg" alt="Card image"/>
+               <Card.Img src="/camping-tents.jpg" alt="Card image"/>
                <Card.ImgOverlay>
-                <Card.Title>About the Blog</Card.Title>
+                <Card.Title style={{textAlign: 'center'}}>About the Blog</Card.Title>
+                <Card.Body style={{textAlign: 'center'}}>
+                    <Card.Text>
+                        It's our goal to provide accurate information. Sharing stories of adventure and travel is important to us. One of the purposes of this blog is to get more people outdoors. 
+                    </Card.Text>
+                </Card.Body>
                </Card.ImgOverlay>
             </Card>
         </div>
