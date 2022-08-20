120eeed95233bad8973310e19602f7fb094e6fb4
CKline22 <CKline.1022@gmail.com>
2022-08-20 11:05:00 -0400

    header updated

--- blog/src/components/header.js
+++ blog/src/components/header.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import CardGroup from 'react-bootstrap/CardGroup';
+import Card from 'react-bootstrap/Card';
+
+
 
 function Header () {
     return(
-        <div>Header</div>
-    )
+        <div>
+            <CardGroup>
+                <Card>
+                    <Card.Img src="/camping-tents-2.jpg"/>
+                </Card>
+                <Card>
+                    <Card.Body>
+                        <Card.Title>
+                            Welcome to Our Blog!
+                        </Card.Title>
+                        <Card.Subtitle>
+                            We are all about everything outdoors.
+                        </Card.Subtitle>
+                        <Card.Text>
+                            Read about our trips, adventures, and Recommended places the explore. 
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </CardGroup>
+        </div>
+    );
 }
 
 export default Header;
