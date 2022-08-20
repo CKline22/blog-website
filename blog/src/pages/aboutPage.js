4e74cce49db39079b752ed7e5ff77a20c824810c
CKline22 <CKline.1022@gmail.com>
2022-08-20 11:40:07 -0400

    readme updated

--- blog/src/pages/aboutPage.js
+++ blog/src/pages/aboutPage.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card';
 
 function About () {
     return(
-        <div>About</div>
+        <div>
+            <Card>
+               <Card.Img src="/landscape-tent-water.jpg" alt="Card image"/>
+               <Card.ImgOverlay>
+                <Card.Title>About the Blog</Card.Title>
+               </Card.ImgOverlay>
+            </Card>
+        </div>
     )
 }
 
