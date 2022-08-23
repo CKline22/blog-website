86b67109b9eb45ae6de1c92b78156e034db6ea7c
CKline22 <CKline.1022@gmail.com>
2022-08-22 20:10:49 -0400

    sign up done

--- blog/src/pages/signupPage.js
+++ blog/src/pages/signupPage.js
@@ -5,18 +5,29 @@ import Card from "react-bootstrap/Card";
 
 function SignUp () {
     return(
-        <div>
-            <Card>
-                <Card.Title>Sign Up</Card.Title>
+        <div className='d-flex justify-content-center'>
+            <Card style={{width: "50%"}}>
+                <Card.Title style={{textAlign: 'center'}}>Sign Up</Card.Title>
                 <Card.Body>
                     <Form>
-                        <Form.Group>email</Form.Group>
-                        <Form.Group>user</Form.Group>
-                        <Form.Group>pass</Form.Group>
+                        <Form.Group className='mb-3' controlId='formBasicEmail'>
+                        <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" placeholder='Enter Email'/>
+                        </Form.Group>
+                        <Form.Group className='mb-3' controlId='formBasicUsername'>
+                        <Form.Label>Username</Form.Label>
+                    <Form.Control type="username" placeholder='Create Username'/>
+                        </Form.Group>
+                        <Form.Group className='mb-3' controlId='formBasicPassword'>
+                        <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder='Create Password'/>
+                        </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicCheckbox">
                             <Form.Check type="checkbox" label="Subscribe to our weekly email" />
                         </Form.Group>
-                        <Button></Button>
+                        <Button variant='secondary' type='submit'>
+                    Join
+                </Button>
                     </Form>
                 </Card.Body>
             </Card>
