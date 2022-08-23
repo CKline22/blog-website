cf2f0a152d2d9cd971e96752ca8923e24d5dd0e0
CKline22 <CKline.1022@gmail.com>
2022-08-22 20:01:39 -0400

    sign up page started

--- blog/src/pages/signupPage.js
+++ blog/src/pages/signupPage.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Card from "react-bootstrap/Card";
 
 function SignUp () {
     return(
-        <div>Sign up now!</div>
+        <div>
+            <Card>
+                <Card.Title>Sign Up</Card.Title>
+                <Card.Body>
+                    <Form>
+                        <Form.Group>email</Form.Group>
+                        <Form.Group>user</Form.Group>
+                        <Form.Group>pass</Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="Subscribe to our weekly email" />
+                        </Form.Group>
+                        <Button></Button>
+                    </Form>
+                </Card.Body>
+            </Card>
+        </div>
     )
 }
 
