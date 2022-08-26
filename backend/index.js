f00b3bfd423f3c1279569ada9b9e9785a7027883
CKline22 <CKline.1022@gmail.com>
2022-08-25 22:16:43 -0400

    updated

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,25 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose')
 
 app.use(express.json())
 app.use(cors())
 
-app.get('/login', (req, res) => {
-    res.send('Login')
+mongoose.connect('mongodb://localhost:27017'/blog-login)
+
+app.post('/api/login', async (req, res) => {
+    console.log(req.body)
+    try {
+        const user = await User.create({
+            username: req.body.username,
+            password: req.body.password,
+        })
+        res.json({status: 'ok'})
+    } catch (err) {
+        res.json({ status: 'error', error: 'wrong password'})
+    }
+    res.json({status: 'ok'})
 })
 
 app.listen(3001, () => {
