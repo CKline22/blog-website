b845fdb2fff24a3360a679fc89ee9aea6fb193d1
CKline22 <CKline.1022@gmail.com>
2022-08-25 20:27:07 -0400

    login backend inprogress

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
-app.get('/test', (req, res) => {
-    res.send('TEST')
+app.use(express.json())
+app.use(cors())
+
+app.get('/login', (req, res) => {
+    res.send('Login')
 })
 
-app.listen(3000, () => {
-    console.log("server started on port 3000")
+app.listen(3001, () => {
+    console.log("server started on port 3001")
 })
--- blog/src/pages/loginPage.js
+++ blog/src/pages/loginPage.js
@@ -2,24 +2,44 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Card from "react-bootstrap/Card";
+import { useState } from "react";
 
 function Login() {
+const [username, setUsername] = useState('')
+const [password, setPassword] = useState('')
+
+async function loginUser (event) {
+  event.preventDefault()
+  const response = await fetch('http://localhost:3001/api/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      username, password,
+    }),
+  })
+
+  const data = await response.json()
+  console.log(data)
+}
+
   return (
     <div className="d-flex justify-content-center">
       <Card style={{ width: "50%" }}>
         <Card.Title style={{ textAlign: "center" }}>Login</Card.Title>
         <Card.Body>
-          <Form>
+          <Form onSubmit={loginUser}>
             <Form.Group className="mb-3" controlId="formBasicUsername">
               <Form.Label>Username</Form.Label>
-              <Form.Control type="username" placeholder="Enter Username" />
+              <Form.Control value={username} onChange={(e) => setUsername(e.target.value)} type="username" placeholder="Enter Username" />
               <Form.Text className="text-muted">
                 Vaild Username or Email accepted
               </Form.Text>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Enter Password" />
+              <Form.Control value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Enter Password" />
             </Form.Group>
             <Button variant="secondary" type="submit">
               Login
